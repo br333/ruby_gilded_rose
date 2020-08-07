58941f5b72d219233b40b12c0698b5413eb2c41c
b3tL06 <bricefrancisco112@gmail.com>
2020-08-08 01:41:58 +0800

    Finished kata

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,57 +1,113 @@
 class GildedRose
 
+  # Items that have rules
+  AGED_BRIE   = 'Aged Brie'
+  SULFURAS    = 'Sulfuras, Hand of Ragnaros'
+  PASS        = 'Backstage'
+  CONJURED    = 'Conjured'
+
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_sell_in(item)
+    item.sell_in -= 1 unless item.sell_in <= 0
+    item.sell_in
+  end
+
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+      update_sell_in(item) unless item.name.eql?(SULFURAS)
+      if item.name.eql?(AGED_BRIE)
+        AgedBrieUpdater.new(item).update!
+      elsif item.name.include?(PASS)
+        BackPassesUpdater.new(item).update!
+      elsif item.name.include?(CONJURED)
+        ConjuredUpdater.new(item).update!
+      elsif item.name.include?(SULFURAS)
+        SulfurasUpdater.new(item).update!
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        ItemUpdater.new(item).update!
       end
     end
   end
 end
+
+
+class ItemUpdater
+  attr_accessor :item
+  # Quality Rules
+  MAX_QUALITY = 50
+  MIN_QUALITY = 0
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update!
+    if item.sell_in <= 0
+      decrease_quality(2)
+    else
+      decrease_quality
+    end
+  end
+
+  private
+    def increase_quality(increase_by = 1)
+      item.quality += increase_by unless item.quality >= MAX_QUALITY
+      item.quality = 50 if item.quality > MAX_QUALITY
+    end
+
+    def decrease_quality(decrease_by = 1)
+      item.quality -= decrease_by unless item.quality <= MIN_QUALITY
+      item.quality = 0 if item.quality < MIN_QUALITY
+    end
+end
+
+class AgedBrieUpdater < ItemUpdater
+  def update!
+    increase_quality
+
+    # if item.sell_in <= 10
+    #   increase_quality(2)
+    # elsif item.sell_in <= 5
+    #   increase_quality(3)
+    # else
+    #   increase_quality
+    # end
+  end
+end
+
+class BackPassesUpdater < ItemUpdater
+  def update!
+    if item.sell_in < 11 && item.sell_in > 5
+      increase_quality(2)
+    elsif item.sell_in < 6 && item.sell_in > 0
+      increase_quality(3)
+    elsif item.sell_in == 0
+      decrease_quality(item.quality)
+    else
+      increase_quality(1)
+    end
+  end
+end
+
+class ConjuredUpdater < ItemUpdater
+  def update!
+    if item.sell_in <= 0
+      decrease_quality(4)
+    else
+      decrease_quality(2)
+    end
+  end
+end
+
+class SulfurasUpdater < ItemUpdater
+  def update!
+    item.quality = 80
+  end
+end
+
 
 class Item
   attr_accessor :name, :sell_in, :quality
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,13 +1,268 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require File.join(File.dirname(__FILE__), 'gilded_rose_v2')
 
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      items[0].name.should == "fixme"
+    # Sample items
+    let(:normal_item)         { Item.new(name="+5 Dexterity Vest", sell_in= 10, quality= 25) }
+    let(:sulfuras_item)       { Item.new(name= "Sulfuras, Hand of Ragnaros", sell_in= 0, quality= 80) }
+    let(:aged_brie_item)      { Item.new(name= "Aged Brie", sell_in= 10, quality= 0) }
+    let(:conjured_item)       { Item.new(name= "Conjured Mana Cake", sell_in= 10, quality= 15) }
+    let(:backstage_pass_item) { Item.new(name= "Backstage passes to a TAFKAL80ETC concert", sell_in= 10, quality= 20) }
+    let(:days)                { 15 }
+
+    # context "all items" do
+    #    #
+    #   it "lowers the sell in value by 1 once day has passed" do
+    #
+    #   end
+    #
+    # end
+
+    context "when normal item" do
+      let(:gilded_rose_object) { GildedRose.new([normal_item]) }
+
+      it "should not have negative quality value" do
+        normal_item.quality = 1
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(normal_item.quality).not_to be_negative
+        end
+        expect(normal_item.quality).not_to be_negative
+      end
+
+      it "should not have negative sell in value" do
+        normal_item.sell_in = 1
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(normal_item.sell_in).not_to be_negative
+        end
+        expect(normal_item.sell_in).not_to be_negative
+      end
+
+      it "should not have quality that exceeds 50" do
+        normal_item.quality = 49
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(normal_item.quality).not_to be > 50
+        end
+        expect(normal_item.quality).not_to be > 50
+      end
+
+      it "degrades the quality by 1 as time passes" do
+        current_quality = normal_item.quality
+        actual_days     = 3
+        1.upto(actual_days) do |day|
+          gilded_rose_object.update_quality
+          expect(normal_item.quality).to eq (current_quality-day).abs
+        end
+        expect(normal_item.quality).to eq (current_quality-actual_days).abs
+      end
+
+      it "degrades the quality twice once sell in day has passed" do
+        normal_item.sell_in = 1
+        current_quality = normal_item.quality
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(normal_item.quality).to eq ([current_quality-(day * 2), 0].max).abs
+        end
+        expect(normal_item.quality).to eq 0
+      end
     end
+
+    context "when aged brie item" do
+      let(:gilded_rose_object) { GildedRose.new([aged_brie_item]) }
+      it "should not have negative quality value" do
+        aged_brie_item.quality = 1
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(aged_brie_item.quality).not_to be_negative
+        end
+        expect(aged_brie_item.quality).not_to be_negative
+      end
+
+      it "should not have negative sell in value" do
+        aged_brie_item.sell_in = 1
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(aged_brie_item.sell_in).not_to be_negative
+        end
+        expect(aged_brie_item.sell_in).not_to be_negative
+      end
+
+      it "should not have quality that exceeds 50" do
+        aged_brie_item.quality = 49
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(aged_brie_item.quality).not_to be > 50
+        end
+        expect(aged_brie_item.quality).not_to be > 50
+      end
+
+      it "upgrades by 1 as time passes" do
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(aged_brie_item.quality).to eq day
+        end
+        expect(aged_brie_item.quality).to eq days
+      end
+    end
+
+    context "when backstage pass item" do
+      let(:gilded_rose_object) { GildedRose.new([backstage_pass_item]) }
+      it "should not have negative quality value" do
+        backstage_pass_item.quality = 1
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(backstage_pass_item.quality).not_to be_negative
+        end
+        expect(backstage_pass_item.quality).not_to be_negative
+      end
+
+      it "should not have negative sell in value" do
+        backstage_pass_item.sell_in = 1
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(backstage_pass_item.sell_in).not_to be_negative
+        end
+        expect(backstage_pass_item.sell_in).not_to be_negative
+      end
+
+      it "should not have quality that exceeds 50" do
+        backstage_pass_item.quality = 49
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(backstage_pass_item.quality).not_to be > 50
+        end
+        expect(backstage_pass_item.quality).not_to be > 50
+      end
+
+      it "upgrades by 1 as time passes when there's more than 10 days to sell" do
+        current_quality = backstage_pass_item.quality
+        backstage_pass_item.sell_in = 15
+        1.upto(3) do |day|
+          gilded_rose_object.update_quality
+          expect(backstage_pass_item.quality).to eq (current_quality + day)
+        end
+        expect(backstage_pass_item.quality).to eq (current_quality + 3)
+      end
+
+      it "upgrades by 2 when there are 10 days or less left to sell and 5 days before the concert" do
+        backstage_pass_item.sell_in = 10
+        current_quality             = backstage_pass_item.quality
+        actual_days                 = 4
+        1.upto(actual_days) do |day|
+          gilded_rose_object.update_quality
+          expect(backstage_pass_item.quality).to eq (current_quality + (day * 2))
+        end
+        expect(backstage_pass_item.quality).to eq (current_quality + (actual_days * 2))
+      end
+
+      it "upgrades by 3 when there are 5 days or less left to sell" do
+        backstage_pass_item.sell_in = 5
+        current_quality             = backstage_pass_item.quality
+        actual_days                 = 3
+        1.upto(actual_days) do |day|
+          gilded_rose_object.update_quality
+          expect(backstage_pass_item.quality).to eq (current_quality + (day * 3))
+        end
+        expect(backstage_pass_item.quality).to eq (current_quality + (actual_days * 3))
+      end
+
+      it "has no quality after the sell in date" do
+        backstage_pass_item.sell_in = 0
+        gilded_rose_object.update_quality
+        expect(backstage_pass_item.quality).to eq 0
+      end
+    end
+
+    context "when conjured item" do
+
+      it "should not have negative quality value" do
+        conjured_item.quality = 1
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(conjured_item.quality).not_to be_negative
+        end
+        expect(conjured_item.quality).not_to be_negative
+      end
+
+      it "should not have negative sell in value" do
+        conjured_item.sell_in = 1
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(conjured_item.sell_in).not_to be_negative
+        end
+        expect(conjured_item.sell_in).not_to be_negative
+      end
+
+      it "should not have quality that exceeds 50" do
+        conjured_item.quality = 49
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(conjured_item.quality).not_to be > 50
+        end
+        expect(conjured_item.quality).not_to be > 50
+      end
+
+      let(:gilded_rose_object) { GildedRose.new([conjured_item]) }
+      it "degrades faster than regular item as time passes" do
+        actual_days     = 5
+        current_quality = conjured_item.quality
+        1.upto(actual_days) do |day|
+          gilded_rose_object.update_quality
+          expect(conjured_item.quality).to eq ([current_quality-(day * 2), 0].max).abs
+        end
+        expect(conjured_item.quality).to eq ([current_quality-(actual_days * 2), 0].max).abs
+      end
+
+      it "degrades twice as fast after the sell in date" do
+        current_quality       = conjured_item.quality
+        conjured_item.sell_in = 1
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(conjured_item.quality).to eq ([current_quality-(day * 4), 0].max).abs
+        end
+        expect(conjured_item.quality).to eq 0
+      end
+    end
+
+    context "when sulfuras item" do
+
+      it "should not have negative quality value" do
+        sulfuras_item.quality = 1
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(sulfuras_item.quality).not_to be_negative
+        end
+        expect(sulfuras_item.quality).not_to be_negative
+      end
+
+      it "should not have negative sell in value" do
+        sulfuras_item.sell_in = 1
+        1.upto(days) do |day|
+          gilded_rose_object.update_quality
+          expect(sulfuras_item.sell_in).not_to be_negative
+        end
+        expect(sulfuras_item.sell_in).not_to be_negative
+      end
+
+      let(:gilded_rose_object) { GildedRose.new([sulfuras_item]) }
+      it "should have a quality of 80" do
+        sulfuras_item.quality = 20
+        gilded_rose_object.update_quality
+        expect(sulfuras_item.quality).to eq 80
+      end
+
+      it "can't be altered" do
+        sulfuras_item.quality = 20
+        1.upto(days) do
+          gilded_rose_object.update_quality
+        end
+        expect(sulfuras_item.quality).to eq 80
+      end
+    end
+
   end
 
 end
